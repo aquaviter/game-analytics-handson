9968d0e9f45b8c852e087a6a0437c29825202a84
Yoshida <hideyy@784f439e4e8b.ant.amazon.com>
2017-09-20 10:51:04 +0900

    add transaction_table.sql

---
+++ transaction_table.sql
@@ -0,0 +1,10 @@
+
+-- Transaction Table
+CREATE TABLE transaction (
+  transaction_id VARCHAR(40) primary key,
+  event_code VARCHAR(10) not null,
+  item_id VARCHAR(10) not null,
+  user_id VARCHAR(10) not null,
+  eventTimestamp DATE not null,
+  platform VARCHAR(10) not null
+);
